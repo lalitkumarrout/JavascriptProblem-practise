b2332842270f21ed6092815aa68349b05dfe884f
Lalit kumar Rout <routlalit172@gmail.com>
2024-12-03 09:31:40 +0530

    closure

---
+++ Closure.js
@@ -0,0 +1,9 @@
+
+// Closures are one of the essential concepts of JavaScript that make the
+// language very flexible and resilient to work with, having a good
+// understanding of it is really important.
+
+
+
+
+
